3b901d45023c61de544ccd0cdfc2b6bfda74d13a
IkyIsaac <isaackitomari33@gmail.com>
2024-09-28 23:52:16 +0300

    Add functions for api calls,test useQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { useQuery } from '@tanstack/react-query'
+import { fetchPosts } from './api/api'
 
 const App = () => {
+  const { data,isLoading }=useQuery({
+    queryKey:["posts"],
+    queryFn: fetchPosts,
+  })
+
+  console.log(data,isLoading)
+
   return (
     <div>hello world</div>
   )
